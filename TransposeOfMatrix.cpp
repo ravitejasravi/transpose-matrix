87cb1a13fcb140786783580e15871eaba7ea6b8e
Raviteja S <43201927+ravitejasravi@users.noreply.github.com>
2022-12-04 20:36:08 +0530

    Solution

---
+++ TransposeOfMatrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        
+        vector<vector<int>> trans;
+
+        for(int i = 0; i < matrix[0].size(); i++) {
+
+            vector<int> temp;
+
+            for(int j = 0; j < matrix.size(); j++) 
+                temp.push_back(matrix[j][i]);
+
+            trans.push_back(temp);
+    
+        }
+        return trans;
+    }
+};
